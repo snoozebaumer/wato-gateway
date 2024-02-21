54eca6fa7ae12eaea77e7de07df6a4f2c14f17c1
Samuel Nussbaumer <samuel.nussbaumer@outlook.com>
2024-02-21 17:45:29 +0100

    new request to get challenge, minor refactoring - dates in log entries - challenge -> challenges due to REST requirements snoozebaumer/wato#3

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const bodyParser = require("body-parser");
 const express = require("express");
-const axios = require('axios');
-require('dotenv').config();
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
+const axios = require("axios");
+require("dotenv").config();
+require("log-timestamp");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 const server = express();
 const port = 8080;
@@ -16,8 +17,8 @@ server.get('/', async(req, res) => {
     res.send("wato API is running.");
 });
 
-server.post('/api/challenge', async(req, res) => {
-    const { challenge, name } = req.body;
+server.post('/api/challenges', async(req, res) => {
+    const { challenge, name, challengeStatus  } = req.body;
 
     // input validation
     if (!challenge || !name) {
@@ -35,7 +36,7 @@ server.post('/api/challenge', async(req, res) => {
             console.log(`GATEWAY: Created user: ${name} with id: ${userId} from IP: ${req.ip}`);
         } catch (error) {
             console.error(`GATEWAY: failed to create user: ${name} from IP: ${req.ip} with error: ${error.message}`);
-            return res.status(500).send('Internal Server Error');
+            return res.status(500).send("Internal Server Error");
         }
     } else {
         userId = req.cookies.id;
@@ -43,12 +44,45 @@ server.post('/api/challenge', async(req, res) => {
 
     // Send request to game service
     try {
-        const response = await axios.post(process.env.GAME_SERVICE_ADDR, { "challenger": userId, challenge });
+        const response = await axios.post(process.env.GAME_SERVICE_ADDR, { "challengerId": userId, challenge, challengeStatus });
         const gameId = response.data.id;
-        console.log("GATEWAY: Created challenge with id: ${gameId} from IP: " + req.ip);
+        console.log(`GATEWAY: Created challenge with id: ${gameId} from IP: ` + req.ip);
         res.send({ id: gameId });
     } catch (error) {
-        console.error("GATEWAY: failed to create challenge from IP: ${req.ip} with error: ", error.message);
+        console.error(`GATEWAY: failed to create challenge from IP: ${req.ip} with error: `, error.message);
+        return res.status(500).send("Internal Server Error");
+    }
+});
+
+server.get('/api/challenges/:id', async(req, res) => {
+    const id = req.params.id;
+
+    try {
+        const response = await axios.get(process.env.GAME_SERVICE_ADDR + "/"+ id);
+
+        // "security": don't send numbers to client if game isn't finished so cheaters can't read from js
+        if (response.data.challengeStatus !== "GAME.FINISHED_SUCCESSFULLY_TITLE" ||
+            response.data.challengeStatus !== "GAME.FINISHED_NOTHING_HAPPENS_TITLE" ) {
+            delete response.data["challengerNumber"];
+            delete response.data["challengeeNumber"];
+        }
+
+        // _id -> id
+        delete Object.assign(response.data, {id: response.data._id })._id;
+
+        try {
+            const challengerUserResponse = await axios.get(process.env.USER_SERVICE_ADDR + "/"+
+                response.data.challengerId);
+            response.data.challengerName = challengerUserResponse.data.name;
+        } catch {
+            response.data.challengerName = "Unknown";
+        }
+
+
+        console.log(`GATEWAY: Fetched challenge with id: ${id} for IP: ` + req.ip);
+        res.send(response.data);
+    } catch (error) {
+        console.error(`GATEWAY: failed to fetch challenge for IP: ${req.ip} with error: `, error.message);
         return res.status(500).send("Internal Server Error");
     }
 });
